121ad5c6c3cd890dc9feec7b32a498ecd98b3e93
Adam Jaamour <dev@jaamour.com>
2023-03-13 21:02:50 +0000

    Build 2S scene with blocks and a player

---
+++ Player.gd
@@ -0,0 +1,15 @@
+extends KinematicBody2D
+
+
+# Declare member variables here
+var velocity = Vector2.ZERO
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("Hello World!")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
