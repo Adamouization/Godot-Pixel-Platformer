8dda6ea80598bc2e41e839a46dc4c147abb42154
Adam Jaamour <dev@jaamour.com>
2023-03-15 17:29:32 +0000

    Add constants

--- Player.gd
+++ Player.gd
@@ -1,12 +1,22 @@
 extends KinematicBody2D
 
 
-# Declare member variables here
+# Member variables declarations
 var velocity = Vector2.ZERO
+
+# Constants
+export(int) var JUMP_HEIGHT = -160
+export(int) var JUMP_RELEASE_HEIGHT = -35
+export(int) var MAX_SPEED = 75
+export(int) var ACCELERATION = 10
+export(int) var FRICTION = 10
+export(int) var GRAVITY = 5
+export(int) var GRAVITY_ACC = 3
 
 # Called when the node enters the scene tree for the first time.
 #func _ready():
 #	print("Hello World!")
+
 
 # Called during every physics frame of the game (default 60).
 # Delta = 1/60
@@ -23,28 +33,32 @@ func _physics_process(delta):
 		apply_acceleration(input.x)
 
 	
-	# Jump
+	# Jumping
+	# Big jump
 	if is_on_floor():
-		if Input.is_action_just_pressed("ui_up"):
-			velocity.y -= 120
+		if Input.is_action_pressed("ui_up"):  # is_action_just_pressed to avoid bouncing directly by holding
+			velocity.y = JUMP_HEIGHT
+	# Small jump
 	else:
-		if Input.is_action_just_released("ui_up") and velocity.y < -30:
-			velocity.y -= 0
+		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_RELEASE_HEIGHT:
+			velocity.y = JUMP_RELEASE_HEIGHT
+		if velocity.y > 0:   # Just started falling (0 = apex)
+			velocity.y += GRAVITY_ACC
 	
 	# Update position
 	velocity = move_and_slide(velocity, Vector2.UP)
 
 
 func apply_gravity():
-	velocity.y += 4
+	velocity.y += GRAVITY
 
 
 func apply_friction():
-	velocity.x = move_toward(velocity.x, 0, 10)
+	velocity.x = move_toward(velocity.x, 0, FRICTION)
 
 
 func apply_acceleration(amount):
-	velocity.x = move_toward(velocity.x, 50 * amount, 10)
+	velocity.x = move_toward(velocity.x, MAX_SPEED * amount, ACCELERATION)
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
