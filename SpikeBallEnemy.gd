47463488ab60a6fecbaaadb352e2d3dfef130a96
Adam Jaamour <dev@jaamour.com>
2023-04-16 08:48:38 +0100

    Editable animation params + convert to tool script

---
+++ SpikeBallEnemy.gd
@@ -0,0 +1,39 @@
+tool
+extends Path2D
+
+
+enum AMIMATION_TYPE {
+	LOOP,
+	BOUNCE
+}
+
+
+# Export vards
+export(AMIMATION_TYPE) var animation_type setget set_animation_type
+export(int) var animation_speed = 1
+
+
+# Shortcuts (: for cast typing and auto-completion)
+onready var animationPlayer: = $AnimationPlayer 
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	play_updated_animation(animationPlayer)
+
+
+func play_updated_animation(ap):
+	match animation_type:
+		AMIMATION_TYPE.LOOP: 
+			ap.play("MoveAlong2DPathLoop")
+		AMIMATION_TYPE.BOUNCE: 
+			ap.play("MoveAlong2DPathBounce")
+	ap.playback_speed = animation_speed
+
+
+func set_animation_type(value):
+
+	animation_type = value
+	var ap = find_node("AnimationPlayer")
+	if ap:
+		play_updated_animation(ap)
