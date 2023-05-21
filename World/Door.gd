a173085ad057da2e36c77eb6c4cca844fe70a1df
Adam Jaamour <dev@jaamour.com>
2023-05-21 12:32:53 +0200

    Finish level transitioning by activating door with space bar

--- World/Door.gd
+++ World/Door.gd
@@ -4,15 +4,28 @@ extends Area2D
 export (String, FILE, "*.tscn") var target_level_path = ""
 
 
-func _on_Door_body_entered(body):
-	# Exit if collision is with anything that is not a player.
-	if not body is Player:
+var player = false
+
+
+func _process(delta):
+	# Don't exit level if player is not over the door
+	if not player:
 		return
 	
-	# Exit if target level path is empty
-	if target_level_path.empty():
+	# Don't exit level if player is not on the floor
+	if not player.is_on_floor():
 		return
 	
+	if Input.is_action_just_pressed("ui_accept"):  # space key
+		# If target level path is empty then exit func	tion
+		if target_level_path.empty():
+			return
+		
+		# Else go to next level as intended
+		go_to_next_level()
+
+
+func go_to_next_level():
 	# Level transition animation
 	LevelTransition.play_exit_level_transition()
 	get_tree().paused = true  # pause game
@@ -22,3 +35,18 @@ func _on_Door_body_entered(body):
 	
 	# Change to a new scene defined in an instance of the door in a scene
 	get_tree().change_scene(target_level_path)
+
+
+func _on_Door_body_entered(body):
+	# Exit if collision is with anything that is not a player.
+	if not body is Player:
+		return
+	
+	player = body
+
+
+func _on_Door_body_exited(body):
+	if not body is Player:
+		return
+	
+	player = null
