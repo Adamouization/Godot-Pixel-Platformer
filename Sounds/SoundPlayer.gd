1ad40625ceff940bebb4c035d180f06b87d378f9
Adam Jaamour <dev@jaamour.com>
2023-05-22 08:27:10 +0200

    Move sound effects to their own subfolder

--- Sounds/SoundPlayer.gd
+++ Sounds/SoundPlayer.gd
@@ -3,10 +3,10 @@ extends Node
 
 # Load sound effects resources
 # Created using https://sfbgames.itch.io/chiptone
-const HURT = preload("res://Sounds/hurt.wav")
-const JUMP = preload("res://Sounds/jump.wav")
-const STOMP_IMPACT = preload("res://Sounds/stomp_enemy_impact.wav")
-const CHECKPOINT = preload("res://Sounds/checkpoint.wav")
+const HURT = preload("res://Sounds/Sound_Effects/hurt.wav")
+const JUMP = preload("res://Sounds/Sound_Effects/jump.wav")
+const STOMP_IMPACT = preload("res://Sounds/Sound_Effects/stomp_enemy_impact.wav")
+const CHECKPOINT = preload("res://Sounds/Sound_Effects/checkpoint.wav")
 
 
 # Shortcut
