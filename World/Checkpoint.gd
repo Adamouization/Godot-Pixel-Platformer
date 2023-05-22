35ee196899479dc74862fd22964cd0e3969248fb
Adam Jaamour <dev@jaamour.com>
2023-05-22 08:19:07 +0200

    Only play sound the first time a checkpoint is reached

--- World/Checkpoint.gd
+++ World/Checkpoint.gd
@@ -5,6 +5,7 @@ onready var animatedSprite: = $AnimatedSprite
 
 
 var active = true
+var is_flag_raised: bool = false
 
 
 func _on_Checkpoint_body_entered(body):
@@ -17,8 +18,13 @@ func _on_Checkpoint_body_entered(body):
 		return
 	
 	# Play animation
-	SoundPlayer.play_sound(SoundPlayer.CHECKPOINT)
 	animatedSprite.play("checked")
+	
+	# Play sound
+	if not is_flag_raised:
+		SoundPlayer.play_sound(SoundPlayer.CHECKPOINT)
+		is_flag_raised = true
+		
 	
 	# Save location as new spawn
 	Events.emit_signal(
