d57679f542b02e01d2b9bac8ed411644adf7f466
Adam Jaamour <dev@jaamour.com>
2023-05-24 20:28:48 +0200

    Improve stomp enemy:
    * play impact sound only if player close enough
    * fine-tune fall/rise speeds and waiting times
    * export vars for easier control

--- Enemies/StompEnemy.gd
+++ Enemies/StompEnemy.gd
@@ -9,6 +9,7 @@ enum {
 }
 
 var state = HOVER
+var is_player_close = false
 
 
 onready var start_position = position   # use instead of global_position
@@ -16,6 +17,12 @@ onready var timer := $Timer
 onready var ray_cast := $RayCast2D
 onready var animated_sprite := $AnimatedSprite
 onready var particles := $Particles2D
+onready var player = get_node("res://Player/Player.tscn")
+
+
+export(int) var FALL_SPEED = 150
+export(int) var RISE_SPEED = 30
+export(float) var WAIT_TIMER = 1.0
 
 
 func _ready():
@@ -35,7 +42,8 @@ func _physics_process(delta):
 
 
 func hover_state():
-	state = FALL
+	if timer.time_left == 0:
+		state = FALL
 
 
 func rise_state(delta):
@@ -43,28 +51,43 @@ func rise_state(delta):
 	animated_sprite.play("rising")
 	
 	# Move back towards start position
-	position.y = move_toward(position.y, start_position.y, 20 * delta)
+	position.y = move_toward(position.y, start_position.y, RISE_SPEED * delta)
 	if position.y == start_position.y:
 		state = HOVER
+	
+	# Wait 1s in land state before entering rise state
+	timer.start(WAIT_TIMER)
 
 
 func fall_state(delta):
 	# Fall down with angry face animation
-	position.y += 100 * delta
+	position.y += FALL_SPEED * delta
 	animated_sprite.play("falling")
 	
 	# Check collision with floor
 	if ray_cast.is_colliding():
-		SoundPlayer.play_sound(SoundPlayer.STOMP_IMPACT)
+		# If player close enough to enemy, play sound (don't play if out of screen)
+		if is_player_close:
+			SoundPlayer.play_sound(SoundPlayer.STOMP_IMPACT)
 		var collision_point = ray_cast.get_collision_point()
 #		position.y = collision_point.y
 		state = LAND
 		
 		# Wait 1s in land state before entering rise state
-		timer.start(1.0)
+		timer.start(WAIT_TIMER)
 		particles.emitting = true
 
 
 func land_state():
 	if timer.time_left == 0:
 		state = RISE
+
+
+func _on_Sound_Impact_Vicinity_body_entered(body):
+	if body is Player:
+		is_player_close = true
+
+
+func _on_Sound_Impact_Vicinity_body_exited(body):
+	if body is Player:
+		is_player_close = false
